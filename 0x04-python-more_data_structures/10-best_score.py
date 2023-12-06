89002aa307899b4953323e564395f1c1f253c00b
liyaayalew <liyamekbib16@gmail.com>
2023-12-06 09:57:59 -0800

    10-best_score.py

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
-def best_score(my_dict):
-    return max(my_dict, key=my_dict.get) if my_dict else None
+def best_score(a_dictionary):
+    if not a_dictionary:
+        return (None)
 
+    return (max(a_dictionary, key=a_dictionary.get))
